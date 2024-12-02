17e5c34de76aa17f1113093fae0d44b1bb4ed91b
IliHanSoLow <ilian-ode@gmx.de>
2024-12-02 11:32:45 +0100

    added header file

--- final.c
+++ final.c
@@ -1,25 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <ctype.h>
-
-typedef struct { // Datenstruktur für das Datum
-    int tag;
-    int monat;
-    int jahr;
-} Datum;
-
-typedef struct { // Datenstruktur für Studenten
-    char nachname[51];
-    char vorname[51];
-    char studiengang[51];
-    char matrikelnummer[9];
-    Datum geburtstag;
-    Datum startdatum;
-    Datum enddatum;
-    struct Student *nextStudent; // Zuweisung des Zeigers auf die nächste Studentenstruktur
-} Student;
+#include "final.h"
 
 Student *head = NULL;
 
